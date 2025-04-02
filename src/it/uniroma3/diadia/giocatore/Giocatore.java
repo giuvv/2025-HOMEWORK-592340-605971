028270ada786bfac29dd957a5a66dc0b9bebe0d7
giuvv <valentinigiulia.04@gmail.com>
2025-04-02 15:21:45 +0200

    giocatore e partita test

--- src/it/uniroma3/diadia/giocatore/Giocatore.java
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -1,7 +1,7 @@
 package it.uniroma3.diadia.giocatore;
 
 import it.uniroma3.diadia.attrezzi.Attrezzo;
-import it.uniroma3.diadia.Borsa;  // Assicurati di importare la classe Borsa
+import it.uniroma3.diadia.giocatore.Borsa;
 
 public class Giocatore {
 
@@ -24,38 +24,9 @@ public class Giocatore {
         this.cfu = cfu;
     }
 
-    // Aggiungi un attrezzo alla borsa
-    public boolean addAttrezzo(Attrezzo attrezzo) {
-        return this.borsa.addAttrezzo(attrezzo);
-    }
-
-    // Rimuovi un attrezzo dalla borsa
-    public Attrezzo removeAttrezzo(String nomeAttrezzo) {
-        return this.borsa.removeAttrezzo(nomeAttrezzo);
-    }
-
-    // Verifica se la borsa contiene un attrezzo
-    public boolean hasAttrezzo(String nomeAttrezzo) {
-        return this.borsa.hasAttrezzo(nomeAttrezzo);
-    }
-
     // Restituisce la borsa del giocatore
     public Borsa getBorsa() {
         return this.borsa;
     }
 
-    // Metodo per verificare se il giocatore è ancora in vita (cioè ha CFU positivi)
-    public boolean isVivo() {
-        return this.cfu > 0;
-    }
-
-    // Metodo per ridurre i CFU del giocatore
-    public void riduciCfu() {
-        this.cfu--;
-    }
-
-    // Metodo per vedere lo stato della borsa
-    public String toString() {
-        return "CFU: " + this.cfu + ", " + this.borsa.toString();
-    }
 }
