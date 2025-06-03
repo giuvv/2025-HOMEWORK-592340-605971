45a3cfe068ac3dbfd4f61049b3c4facebc18dda8
giuvv <valentinigiulia.04@gmail.com>
2025-06-03 19:26:45 +0200

    comandi

--- src/it/uniroma3/diadia/comandi/ComandoPrendi.java
+++ src/it/uniroma3/diadia/comandi/ComandoPrendi.java
@@ -1,43 +1,33 @@
 package it.uniroma3.diadia.comandi;
 
-import it.uniroma3.diadia.IOConsole;
+import it.uniroma3.diadia.IO;
 import it.uniroma3.diadia.Partita;
-import it.uniroma3.diadia.ambienti.Stanza;
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 
-public class ComandoPrendi implements Comando {
-	
-	private String nomeAttrezzo;
-	IOConsole ioConsole = new IOConsole();
+public class ComandoPrendi extends AbstractComando {
+
+	private final static String NOME = "prendi";
+
 	
 	@Override
 	public void esegui(Partita partita) {
-		Stanza stanza = partita.getStanzaCorrente();
-		Attrezzo attrezzo = stanza.getAttrezzo(nomeAttrezzo);
-			
-			if (attrezzo==null)
-				ioConsole.mostraMessaggio("L'attrezzo " + attrezzo + " non è nella stanza");
-			else if (partita.getGiocatore().getBorsa().addAttrezzo(attrezzo)) {
-				stanza.removeAttrezzo(attrezzo);
-				ioConsole.mostraMessaggio("Hai preso: " + attrezzo);
+		Attrezzo a = partita.getLabirinto().getStanzaCorrente().getAttrezzo(this.getParametro());
+		if(a==null) {
+			this.getIo().mostraMessaggio("Attrezzo non presente nella stanza!");
+		} 
+		else {
+			if(partita.getGiocatore().getBorsa().getPesoRimanente(a)) {
+				partita.getGiocatore().getBorsa().addAttrezzo(a);
+				partita.getLabirinto().getStanzaCorrente().removeAttrezzo(a);
+			} 
+			else
+				this.getIo().mostraMessaggio("Attrezzo troppo pesante per entrare nella borsa!");
 			}
-			else 
-				ioConsole.mostraMessaggio("La borsa è già piena");
 	}
-	
-	@Override
-	public void setParametro(String parametro) {
-		this.nomeAttrezzo = parametro;		
-	}
-	
+
 	@Override
 	public String getNome() {
-		return null;
+		return NOME;
 	}
-	
-	@Override
-	public String getParametro() {
-		return this.nomeAttrezzo;
-	}
-	
+
 }
--- src/it/uniroma3/diadia/comandi/ComandoVai.java
+++ src/it/uniroma3/diadia/comandi/ComandoVai.java
@@ -1,47 +1,46 @@
 package it.uniroma3.diadia.comandi;
 
 import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.Direzione;
 import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.giocatore.Giocatore;
 
-public class ComandoVai implements Comando {
-private String direzione;
-
-@Override
-public void esegui(Partita partita) {
-	Stanza stanzaCorrente = partita.getStanzaCorrente();
-	Stanza prossimaStanza = null;
-	if (this.direzione==null) {
-		System.out.println("Dove vuoi andare?");
-		System.out.println("Devi specificare una direzione");
-		return;			
-	}
+public class ComandoVai extends AbstractComando {
 	
-	prossimaStanza = stanzaCorrente.getStanzaAdiacente(this.direzione);
-	if (prossimaStanza==null) {
-		System.out.println("Direzione inesistente");
-		return;
+	private final static String NOME = "vai";
+
+	/**
+	 * esecuzione del comando
+	 */
+	@Override
+	public void esegui(Partita partita) {
+		Stanza stanzaCorrente = partita.getStanzaCorrente();
+		Stanza prossimaStanza = null;
+		if (this.getParametro() == null) {
+			this.getIo().mostraMessaggio("Dove vuoi andare? Devi specificare una direzione");
+		}
+		if(this.getParametro()!=null )// && (Direzione.valueOf(this.getParametro()).getClass() != Direzione.class))
+			try {
+			prossimaStanza = stanzaCorrente.getStanzaAdiacente(Direzione.valueOf(this.getParametro()));
+			} catch(IllegalArgumentException e) {
+				this.getIo().mostraMessaggio("Direzione inesistente");
+				return;
+			}
+			
+			if (prossimaStanza == null) {
+			this.getIo().mostraMessaggio("Direzione inesistente");
+			return;
+		}
+
+		partita.setStanzaCorrente(prossimaStanza);
+		this.getIo().mostraMessaggio(partita.getStanzaCorrente().getNome());
+		Giocatore giocatore = partita.getGiocatore();
+		giocatore.setCfu(giocatore.getCfu() - 1);
 	}
-	
-	partita.setStanzaCorrente(prossimaStanza);
-	System.out.println(partita.getStanzaCorrente().getNome());
-	partita.getGiocatore().setCfu(partita.getGiocatore().getCfu()-1);
-}
 
-@Override
-public void setParametro(String parametro) {
-	this.direzione = parametro;
-}
-
-@Override
-public String getNome() {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public String getParametro() {
-	// TODO Auto-generated method stub
-	return null;
-}
+	@Override
+	public String getNome() {
+		return NOME;
+	}
 
 }
