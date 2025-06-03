fb1ee59e2ce71206df15dc5da4fbd9159f689930
camu03 <camussifilippo@gmail.com>
2025-06-03 19:07:58 +0200

    comando

--- src/it/uniroma3/diadia/comandi/Comando.java
+++ src/it/uniroma3/diadia/comandi/Comando.java
@@ -1,5 +1,6 @@
 package it.uniroma3.diadia.comandi;
 
+import it.uniroma3.diadia.IO;
 import it.uniroma3.diadia.Partita;
 
 public interface Comando {
@@ -10,4 +11,6 @@ public interface Comando {
 	public String getNome();
 	
 	public String getParametro();
+	
+	public void setIo(IO io);
 }
